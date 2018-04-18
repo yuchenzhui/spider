794a29cc4d2275fdbcc1e4a4524597e638860a9e
yuchenzhui <34471109+yuchenzhui@users.noreply.github.com>
2018-04-18 14:57:21 +0800

    Initial commit

---
+++ apidertest/apidertest/items.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+# Define here the models for your scraped items
+#
+# See documentation in:
+# https://doc.scrapy.org/en/latest/topics/items.html
+
+import scrapy
+
+class ApidertestItem(scrapy.Item):
+    # define the fields for your item here like:
+    num = scrapy.Field()
+    pass
---
+++ apidertest/apidertest/spiders/__init__.py
@@ -0,0 +1,11 @@
+# This package will contain the spiders of your Scrapy project
+#
+# Please refer to the documentation for information on how to create and manage
+# your spiders.
+
+import re
+import scrapy
+
+from scrapy.http import request
+from apidertest.items import ApidertestItem
+
